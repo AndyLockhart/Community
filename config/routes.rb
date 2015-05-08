4fb927ae10b7e928bb90ec941b3cb0d25853885c
AndyLockhart <andylockhart@AndyLockhartdeMacBook-Pro.local>
2015-05-09 02:05:58 +0800

    chapter 14,finish rails-10 happy peter

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  root 'page#welcome'
-  get '/about' => 'page#about'
+  	root 'page#welcome'
+  	get '/about' => 'page#about'
 
-  # issues
-  resources :issues
+  	# issues
+  	resources :issues
+
+  	# comments
+	post '/issues/:issue_id/comments' => "comments#create"
 end
